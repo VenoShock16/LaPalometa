67690026563d9bcb8a9e2bd85c02bc0fd8dce22b
Jorge Méndez <jorgemenmar04@gmail.com>
2023-10-24 12:32:31 +0200

    y

--- Assignment.java
+++ Assignment.java
@@ -7,27 +7,15 @@
  */
 public class Assignment
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private Taxi taxi;
+    private Passenger passenger;
 
     /**
      * Constructor for objects of class assignments
      */
-    public Assignment()
+    public Assignment(Taxi taxi, Passenger passenger)
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        this.taxi= taxi;
+        this.passenger= passenger;
     }
 }
