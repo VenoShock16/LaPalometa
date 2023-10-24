d9545d9bade08043cf62db58cd7f1d726d5eadf1
Jorge Méndez <jorgemenmar04@gmail.com>
2023-10-24 12:36:44 +0200

    y

--- Assignment.java
+++ Assignment.java
@@ -13,9 +13,19 @@ public class Assignment
     /**
      * Constructor for objects of class assignments
      */
+    public Assignment(){
+        taxi= null;
+        passenger= null;
+    }
     public Assignment(Taxi taxi, Passenger passenger)
     {
         this.taxi= taxi;
         this.passenger= passenger;
+    }
+    public Taxi getTaxiAssignment(Taxi taxi){
+        return taxi;
+    }
+    public Passenger getTaxiAssignment(Passenger passenger){
+        return passenger;
     }
 }
