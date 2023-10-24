f9b2ea7a30d8735e4a9171d1ba1ff11cf333e142
VenoShock16 <josedpulido@gmail.com>
2023-10-24 18:26:56 +0200

    s

--- ComparadorNombreTaxi.java
+++ ComparadorNombreTaxi.java
@@ -5,7 +5,7 @@ import java.util.*;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class ComparadorNombreTaxi
+public class ComparadorNombreTaxi implements Comparator<Passenger>
 {
     public int compare(Taxi t1, Taxi t2){  
         return (t1.getName().compareTo(t2.getName()));
